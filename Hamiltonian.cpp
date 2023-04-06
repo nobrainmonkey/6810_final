1015609f99f60f8f48bcdae535e5f204a014ca45
Xihe Han <han.1929@osu.edu>
2023-04-05 22:56:07 -0400

    Initial commit for Hamiltonian header file and source file

---
+++ Hamiltonian.cpp
@@ -0,0 +1,63 @@
+// file: Hamiltonian.cpp
+//
+// Definition for the Hamiltonian namespace functions
+//
+// Programmer: Xihe Han
+//
+// Revision history:
+//	04/05/2023 Origional version 
+//*****************************************************
+
+// include files
+#include "Hamiltonian.h"
+
+// function definition in Hamiltonian.h
+// return the energy calculation of the nearest neighbor for a given element
+double Hamiltonian::hamiltonian_periodic_ising_element(int i, int j, Eigen::MatrixXd *microstate_matrix_ptr, hamiltonian_param_struct *hamiltonian_param){
+	double const J = hamiltonian_param -> J;    //spin-spin interaction constant
+	double const h = hamiltonian_param -> h;    //magnetic field value
+
+	int const rows = microstate_matrix_ptr -> rows();
+	int const cols = microstate_matrix_ptr -> cols();
+
+	// calculate the change in row index for up/down neighbor
+	// and the change in col index for left/right neighbor 
+	int up = (i - 1 + rows) % rows;
+	int down = (i + 1) % rows;
+	int left = (j - 1 + cols) % cols;
+	int right = (j + 1) % cols;
+
+	// apply the equation E_ij = sum<S_ij> - h*S_i. 
+	// this equation comes from the hamiltonian of ising model
+	double element_interaction_energy = 
+		-J * (
+				(*microstate_matrix_ptr)(i,j) * (*microstate_matrix_ptr)(up, j) +  
+				(*microstate_matrix_ptr)(i,j) * (*microstate_matrix_ptr)(down, j) +
+				(*microstate_matrix_ptr)(i,j) * (*microstate_matrix_ptr)(i, left) +
+				(*microstate_matrix_ptr)(i,j) * (*microstate_matrix_ptr)(i, right) 
+			)
+		- h * (*microstate_matrix_ptr)(i,j);
+
+	return element_interaction_energy;
+
+}
+
+// function definition in Hamiltonian.h
+// return the energy calculation for the entire microstate.
+double Hamiltonian::hamiltonian_periodic_ising_microstate(Eigen::MatrixXd *microstate_matrix_ptr, hamiltonian_param_struct *hamiltonian_param){
+	int const rows = microstate_matrix_ptr -> rows();
+	int const cols = microstate_matrix_ptr -> cols();
+
+	double microstate_energy = 0;
+	
+	// go through the microstate 
+	// and sum the energy for each spin.
+	for(int i = 0; i < rows; i++){
+		for(int j = 0; j < cols; j++){
+			microstate_energy += hamiltonian_periodic_ising_element(i, j, microstate_matrix_ptr,  hamiltonian_param);
+		}
+	}
+
+	return microstate_energy;
+
+}
---
+++ Hamiltonian.h
@@ -0,0 +1,40 @@
+//  file Hamiltonian.h
+//
+//  Header file for the Hamiltonian functions
+//
+//  Programmer: Xihe Han
+//
+//  Revision History:
+//		04/05/2023 origional version
+//
+//***********************************************
+
+// ensuring the header file is only defined once
+#ifndef HAMILTONIAN_H
+#define HAMILTONIAN_H
+
+// defining the structure of the hamiltonian parameters.
+// In the case of Ising model, all we need is J and h.
+struct hamiltonian_param_struct{
+	double J;
+	double h;
+};
+
+// include files
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/src/Core/Matrix.h>
+
+// namespace definition and function prototypes
+namespace Hamiltonian{
+	// Define the Hamiltonian function for a single spin in 2-D ising model
+	// This function return the interaction energy of an element at (i-1, j-1)
+	// with its neighbors with periodic boundary conditon.
+	double hamiltonian_periodic_ising_element (int i, int j, Eigen::MatrixXd* microstate_matrix_ptr, hamiltonian_param_struct* hamiltonian_param);
+
+	// Define the Hamiltonian fucntion for the entire microstate of a system of sice 
+	// NxM.
+	double hamiltonian_periodic_ising_microstate (Eigen::MatrixXd* microstate_matrix_ptr, hamiltonian_param_struct* hamiltonian_param);
+	
+};
+
+#endif
